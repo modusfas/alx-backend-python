53fe276b297301349572ffd28b93c897779376e1
Modusfas <modupefasunon@gmail.com>
2024-04-04 11:55:11 +0000

    tests

---
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+TEST_PAYLOAD = [
+    (
+        {"repos_url": "https://api.github.com/orgs/google/repos"},
+        [
+            {
+                "id": 7697149,
+                "node_id": "MDEwOlJlcG9zaXRvcnk3Njk3MTQ5",
+                "name": "episodes.dart",
+                "full_name": "google/episodes.dart",
+                "private": False,
+                "owner": {
+                    "login": "google",
+                    "id": 1342004,
+                    "node_id": "MDEyOk9yZ2FuaXphdGlvbjEzNDIwMDQ=",
+                    "avatar_url": "https://avatars1.githubusercontent.com/u/1342004?v=4",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/google",
+                    "html_url": "https://github.com/google",
